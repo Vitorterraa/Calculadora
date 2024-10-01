5a4f0a080d1b411d94af74e1f6ad2717eca2d8a8
Vitorterraa <vterra2008@gmail.com>
2024-10-01 08:32:36 -0300

    Nomificando os botões e fazendo-os aparecer na tela

--- Jogo.cs
+++ Jogo.cs
@@ -17,14 +17,59 @@ namespace Calculadora
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn1_Click(object sender, EventArgs e)
         {
-
+            visor.Text = visor.Text + "1";
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void btn2_Click(object sender, EventArgs e)
         {
+            visor.Text = visor.Text + "2";
+        }
 
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "9";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            visor.Text = visor.Text + "0";
+        }
+
+        private void btncls_Click(object sender, EventArgs e)
+        {
+            visor.Clear();
         }
     }
 }
